2398cc296749fa90462c56e0158c1a10cdf14591
sundara26071978 <sunjsg@gmail.com>
2026-03-14 20:10:00 +0530

    research agent with tavily search

---
+++ first-deepagent.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+import os
+
+from typing import Literal
+from tavily import TavilyClient
+from deepagents import create_deep_agent
+
+load_dotenv()
+
+os.environ["OLLAMA_API_KEY"]=os.getenv("OLLAMA_API_KEY")
+os.environ["TAVILY_API_KEY"]=os.getenv("TAVILY_API_KEY")
+
+
+
+tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
+
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """Run a web search"""
+    return tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
+
+
+# System prompt to steer the agent to be an expert researcher
+research_instructions = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+
+You have access to an internet search tool as your primary means of gathering information.
+
+## `internet_search`
+
+Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+"""
+
+
+agent = create_deep_agent(
+    model="ollama:qwen3.5",
+    tools=[internet_search],
+    system_prompt=research_instructions,
+    debug=True,
+)
+
+result = agent.invoke({"messages": [{"role": "user", "content": "What is ESG?"}]})
+
+# Print the agent's response
+print(result["messages"][-1].content)
+
+print("Done!")
